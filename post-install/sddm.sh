90504bc4285fc3fcc30c656e07f81dfd074262cb
Stephen Reynolds <mail@stephenreynolds.dev>
2023-05-05 08:57:52 -0400

    feat: add Sugar Candy for SDDM theme

--- post-install/sddm.sh
+++ post-install/sddm.sh
@@ -3,3 +3,11 @@
 set -ouex pipefail
 
 systemctl enable sddm.service
+
+# Theme
+mkdir -p /usr/share/sddm/themes
+git clone https://framagit.org/MarianArlt/sddm-sugar-candyu.git /usr/share/sddm/themes
+cat <<EOF > /usr/lib/sddm/sddm.conf.d/sddm.conf
+[Theme]
+Current=sugar-candy
+EOF

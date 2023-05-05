325841692ea824c5a1e3950a77e9b514fc5e97f9
Stephen Reynolds <mail@stephenreynolds.dev>
2023-05-05 10:05:17 -0400

    revert broken theme

--- post-install/sddm.sh
+++ post-install/sddm.sh
@@ -3,11 +3,3 @@
 set -ouex pipefail
 
 systemctl enable sddm.service
-
-# Theme
-mkdir -p /usr/share/sddm/themes
-git clone https://github.com/MarianArlt/sddm-chili.git /usr/share/sddm/themes/chili
-cat <<EOF > /etc/sddm.conf.d/50-theme.conf
-[Theme]
-Current=chili
-EOF

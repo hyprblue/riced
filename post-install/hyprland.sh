03553002e7c8c449655a6c34e5f5c0af863f1d60
Stephen Reynolds <mail@stephenreynolds.dev>
2023-05-05 05:10:55 -0400

    style: append newline before custom configs

--- post-install/hyprland.sh
+++ post-install/hyprland.sh
@@ -4,6 +4,7 @@
 sed -i 's|^Exec=.*|Exec=/usr/bin/start-hyprland|' /usr/share/wayland-sessions/hyprland.desktop
 
 # Append custom Hyprland configuration to the default config
+echo "\n" >> /usr/share/hyprland/hyprland.conf
 for config in "/usr/share/hyprland/conf.d"/*.conf; do
     echo "source = $config" >> /usr/share/hyprland/hyprland.conf
 done

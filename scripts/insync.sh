184e4173db155b5d74b5d7b1e93f871333e0c803
Stephen Reynolds <mail@stephenreynolds.dev>
2023-05-06 14:00:41 -0400

    fix: add insync repo using script

--- scripts/insync.sh
+++ scripts/insync.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/sh
 
 rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
+
+cat <<EOF > /etc/yum.repos.d/insync.repo
+[insync]
+name=insync repo
+baseurl=http://yum.insync.io/fedora/\$releasever/
+gpgcheck=1
+gpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
+enabled=1
+metadata_expire=120m
+EOF
